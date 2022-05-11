86d49a535b5a39060548a8605613dd2f19ad9209
Santiago Neira <santiagoneira52@gmail.com>
2022-05-11 14:07:18 +0000

    add functions

--- operations.c
+++ operations.c
@@ -6,8 +6,32 @@
 *@line_number: line number
 */
 
-void push(stack_t **head, unsigned int line_number);
+void push(stack_t **head, unsigned int line_number)
+{
+	stack_t *element = NULL;
+	new_node = malloc(sizeof(stack_t));
 
+	if (element == NULL)
+	{
+		return(NULL);
+	}
+
+	/*if tuviera data debería darle el valor*/
+	element->prev = NULL;
+	element->next = NULL;
+	
+	if (*head != NULL)
+	{
+		(*head)->prev = element;
+	}
+
+	if (element != NULL)
+	{
+		element->next = *head;
+		*head = new_node;
+	
+	}
+}
 
 /**
 *pall - print all the elements in the stack
