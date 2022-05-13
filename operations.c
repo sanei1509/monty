84849fa941a806c7c2d443123be639fa73d1ac07
hellerdejanuar <hellerdejanuar@gmail.com>
2022-05-12 19:30:35 -0700

    pchar

--- operations.c
+++ operations.c
@@ -128,16 +128,20 @@ void pchar(stack_t **head, unsigned int line_number __attribute__((unused)))
 {
 	stack_t *c_node = *head;
 
-	if (*head != NULL)
+	if (c_node->n < 127)
+		fprintfi(stderr, "L%u: can't pchar, value out of range\n", line_number);
+	
+	else if (*head != NULL)
 	{
 		printf("%c\n", c_node->n);
+		return;
 	}
 	else
-	{
 		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
-		fclose(gl.fp);
-		free(gl.line_read);
-		exit(EXIT_FAILURE);
-	}
+
+	fclose(gl.fp);
+	free(gl.line_read);
+	exit(EXIT_FAILURE);
+	
 
 }
